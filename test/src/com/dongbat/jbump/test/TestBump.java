967440e3993732759a492d79f3afd252bf22cc08
raeleus <raymond.ray3k@gmail.com>
2020-07-02 08:09:22 -0700

    Added libGDX dependency for testing.

--- test/src/com/dongbat/jbump/test/TestBump.java
+++ test/src/com/dongbat/jbump/test/TestBump.java
@@ -1,7 +1,13 @@
 package com.dongbat.jbump.test;
 
-public class TestBump {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class TestBump extends ApplicationAdapter {
     public static void main(String[] args) {
-        
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 800);
+        new Lwjgl3Application(new TestBump(), config);
     }
 }
